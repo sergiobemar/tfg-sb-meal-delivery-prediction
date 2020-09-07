4b3f9fc54b962d6ace7fab36111937c3f305e830
Sergio <sergiobenitomartin@gmail.com>
2020-09-07 00:17:13 +0000

    Updated

--- src/deploy/serve_model.py
+++ src/deploy/serve_model.py
@@ -1,10 +1,15 @@
 import joblib
-import pandas as pd
 import json
+import os
+import pandas as pd
 
 from flask import Flask
+from flask import jsonify
 from flask import request
 # from flask_restful import Api, Resource
+import sys
+
+os.chdir('/home/jupyter/tfg-sb-meal-delivery-prediction/')
 
 from src.data.data_collect import read_train_data
 from src.model.xgboost_model import preprocess_data, train_xgboost_model
@@ -35,33 +40,70 @@ def get():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-	data = {}
+# 	data = {}
+	
+# 	for v in features:
+# 		data[v] = [request.form[v]]
+	
+# 	print(request.form)
+	content = request.json
+# 	print('Contenido: ' + content)
+		
+# 	json_data_dict = request.get_json(force=True)
+#     X = json_data_dict['features']
 
-	for v in features:
-		data[v] = [request.form[v]]
+	df = pd.DataFrame.from_records(content)
+	
+	print(df.columns)
+	df = df[features]
+# 	df = pd.DataFrame(data)
 
-	data = pd.DataFrame(data)
-
+	df = df.apply(pd.to_numeric)
+	
+	print('NUM ROWS: ' + str(len(df.index)))
 	pred = regressor_model.predict(df[features])
 
-	return pred[0]
+# 	return pred[0]
+# 	result = {
+# 		"features" : features,
+# 		"columns" : list(df.columns),
+# 		"pred" : list(pred[0])
+# 	}
+# 	print(pred)
+	result = {i : str(pred[i]) for i in range(0, len(pred))}
+	return result
+
+@app.route('/save', methods=['GET'])
+def save_model():
+	
+	joblib.dump(regressor_model, './models/xgboost_model.pkl')
+	joblib.dump(features, './models/xgboost_features.pkl')
+	
+	return {'response': 'Model saved done!'}
 
 @app.route('/train', methods=['POST'])
 def train():
-
-	center_id = request.args.get('center_id')
-	meal_id = request.args.get('meal_id')
-
+	
+	# Get data from the request
+	content = request.json
+	
+	center_id = content['center_id']
+	meal_id = content['meal_id']
+	
+	# Preprocess the dataframe
 	df_preprocessed = preprocess_data(df_train, center_id, meal_id)
 	
 	regressor_model, rmse = train_xgboost_model(df_preprocessed)
 
-	features = df.drop(columns='num_orders').columns
+	features = list(df_preprocessed.drop(columns='num_orders').columns)
 
 	result = {
 		'features' : features,
 		'rmse' : rmse
 	}
+# 	result = {
+# 		'data' : content['center_id']
+# 	}
 	return jsonify(result)
 # api.add_resource(Test, '/my_custom_test')
 # api.add_resource(Predict, '/predict')
