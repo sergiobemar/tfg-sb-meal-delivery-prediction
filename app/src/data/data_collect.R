f752f244cd71fc179b775610ef7ff5608f93b3d0
sergiobemar <sergiobenitomartin@gmail.com>
2020-12-29 19:07:35 +0100

    Get data from Clickhouse

--- app/src/data/data_collect.R
+++ app/src/data/data_collect.R
@@ -1,5 +1,43 @@
 source('src/features/preprocessing.R')
 
+get_data_clickhouse <- function() {
+  
+  # Create connection to Clickhouse
+  con_ch <- dbConnect(
+    clickhouse::clickhouse(), 
+    host = credentials_ch$host, 
+    port = credentials_ch$port, 
+    user = credentials_ch$user, 
+    password = credentials_ch$password,
+    database = credentials_ch$database
+  )
+  
+  # Get raw data
+  ## Meal
+  query = "SELECT * FROM raw.meal"
+  output_path <- "./data/clickhouse/meal.csv"
+  dbGetQuery(con_ch, query) %>% as.data.frame() %>% write.csv2(output_path, quote = F, row.names = F)
+  
+  ## Center
+  query = "SELECT * FROM raw.center"
+  output_path <- "./data/clickhouse/center.csv"
+  dbGetQuery(con_ch, query) %>% as.data.frame() %>% write.csv2(output_path, quote = F, row.names = F)
+  
+  ## Train
+  query = "SELECT * FROM raw.test"
+  output_path <- "./data/clickhouse/test.csv"
+  dbGetQuery(con_ch, query) %>% as.data.frame() %>% write.csv2(output_path, quote = F, row.names = F)
+  
+  ## Train
+  query = "SELECT * FROM raw.train"
+  output_path <- "./data/clickhouse/train.csv"
+  dbGetQuery(con_ch, query) %>% as.data.frame() %>% write.csv2(output_path, quote = F, row.names = F)
+  
+  # Disconnect
+  dbDisconnect(con_ch)
+  
+}
+
 get_shiny_data <- function() {
   
   # Train info
@@ -12,6 +50,20 @@ get_shiny_data <- function() {
   # Get center and meal data
   df_center <<- read.csv2(paste0("./data/raw/", "fulfilment_center_info.csv"), sep = ",") %>% as.data.table()
   df_meal <<- read.csv2(paste0("./data/raw/", "meal_info.csv"), sep = ",") %>% as.data.table()
+}
+
+get_shiny_data_ch <- function() {
+  
+  # Train info
+  df_train <<- read.csv2(paste0("./data/clickhouse/", "train.csv"), sep = ";") %>% as.data.table()
+  df_orders_set_up <<- preprocess_dataset(df_train, "train.csv", FALSE)
+  
+  # Predict info
+  df_test <<- read.csv2(paste0("./data/clickhouse/", "test.csv"), sep = ";") %>% as.data.table()
+  
+  # Get center and meal data
+  df_center <<- read.csv2(paste0("./data/clickhouse/", "fulfilment_center_info.csv"), sep = ";") %>% as.data.table()
+  df_meal <<- read.csv2(paste0("./data/clickhouse/", "meal_info.csv"), sep = ";") %>% as.data.table()
 }
 
 get_data_predict <- function(center, meal) {
